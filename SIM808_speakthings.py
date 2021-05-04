97628fbfae491ba6e3f4a7fc65d94bd671e7cf3f
hosseinniri <33494188+hosseinniri@users.noreply.github.com>
2021-05-04 22:15:51 +0430

    upload data ok

---
+++ SIM808_speakthings.py
@@ -0,0 +1,208 @@
+import logging
+import threading
+import json
+import serial   
+import os, time
+import pynmea2
+from subprocess import call
+from time import sleep
+from datetime import datetime
+from pprint import pprint
+
+logging.basicConfig(filename='gps.log', level=logging.DEBUG, filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.debug('Script started')
+
+# Enable Serial Communication
+logging.debug('Enabling serial')
+port = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=5) ## Enter serial port here
+port.write("AT+CGNSTST=0\r\n".encode())  ## Ensure gps stream is off before we start
+
+#activate internet commands
+gprspwr = "AT+CFUN=1\r\n"
+gprsconntype = 'AT+SAPBR=3,1,"Contype","GPRS"\r\n'
+apnset = 'AT+SAPBR=3,1,"APN","mcinet"\r\n' ## Set your APN
+apnuser = 'AT+SAPBR=3,1,"USER","wap"\r\n' ## APN User
+apnpass = 'AT+SAPBR=3,1,"PWD","wap"\r\n' ## APN Password
+startconn = "AT+SAPBR=1,1\r\n"
+getip = "AT+SAPBR=2,1\r\n"
+attachgprs = "AT+HTTPINIT\r\n"
+
+#powerup gps
+gpspowerup = "AT+CGNSPWR=1\r\n"
+
+#startgps
+gpsgo = "AT+CGNSTST=1\r\n"
+#
+#   FIRE UP MODULE -- Turn on gps and start sending readings
+logging.debug("Enabling features")
+port.write(gprspwr.encode())
+res = port.readline().decode('utf-8', errors='replace')
+logging.debug(res)
+time.sleep(1)
+logging.debug("APN Setup")
+port.write(gprsconntype.encode())
+res = port.read(50).decode('utf-8', errors='replace')
+res1 = port.read(50).decode('utf-8', errors='replace')
+
+logging.debug(res)
+logging.debug(res1)
+time.sleep(1)
+
+port.write(apnset.encode())
+res = port.readline().decode('utf-8', errors='replace')
+logging.debug(res)
+time.sleep(1)
+
+port.write(apnuser.encode())
+res = port.readline().decode('utf-8', errors='replace')
+logging.debug(res)
+time.sleep(1)
+
+port.write(apnpass.encode())
+res = port.readline().decode('utf-8', errors='replace')
+logging.debug(res)
+time.sleep(1)
+
+print("Start Connection")
+port.write(startconn.encode())
+res = port.readline().decode('utf-8', errors='replace')
+logging.debug(res)
+time.sleep(1)
+
+logging.debug("Get IP")
+port.write(getip.encode())
+res = port.readline().decode('utf-8', errors='replace')
+logging.debug(res)
+
+port.write(attachgprs.encode())
+res = port.readline().decode('utf-8', errors='replace')
+logging.debug(res)
+logging.debug("GPS Power ON")
+
+port.write(gpspowerup.encode())
+res = port.read(100)
+logging.debug(res)
+time.sleep(1)
+
+logging.debug("GPS Start")
+port.write(gpsgo.encode())
+res = port.read(25).decode('utf-8', errors='replace')
+logging.debug(res)
+
+
+def getTimeAndDate():
+	date = datetime.now()
+	timestr = '{}:{}:{:02d}'.format(date.hour, date.minute, date.second)
+	datestr = '{}-{:02d}-{:02d}'.format(date.year, date.month, date.day)
+	return (timestr, datestr)
+
+def handleGPSmsg(GGAmsg, RMCmsg):
+	"""
+	GGAmsg is a pynmea2 parsed GGA string (http://www.gpsinformation.org/dale/nmea.htm#GGA)
+	To see what fields are available see the pynmea2 docs (https://github.com/Knio/pynmea2)
+	To get latitude in degrees for example you can do msg.latitude
+	"""
+	speed = RMCmsg.spd_over_grnd # speed in knots
+	speed *= 0.514444 # speed in meters/sec
+
+	msg = GGAmsg
+	altitude = str(msg.altitude)
+	timestmp = str(getTimeAndDate()[0] + ', ' + getTimeAndDate()[1])
+
+	longitude = str(msg.longitude)
+	latitude = str(msg.latitude)
+
+	lat_dir=str(msg.lat_dir)
+	lon_dir=str(msg.lon_dir)
+
+	gps_qual=str(msg.gps_qual)
+	gps_num_sats=str(msg.num_sats)
+
+	horizontal_dil=str(msg.horizontal_dil)
+
+	logging.debug('Stop GPS for loop run...')
+	port.write("AT+CGNSTST=0\r\n".encode())
+	time.sleep(2)
+	port.reset_input_buffer()
+	print(timestmp + " Lat: " + latitude + " Long: " + longitude + ' Alt: ' + altitude + ' Meters spd: ' + '{:.3f}'.format(speed) + "m/s")
+    #logging.debug('Set SSL')
+	port.write('AT+HTTPSSL=1\r\n'.encode())
+	response = port.readline().decode('utf-8', errors='replace')
+	if "OK" in response:
+		logging.debug("SSL DONE!")
+	else: logging.error('SSL Error!')
+	
+	logging.debug('Set CID')
+	port.write('AT+HTTPPARA=\"CID\",1\r\n'.encode())
+	response = port.read(100).decode('utf-8', errors='replace')
+    #logging.debug(str(response))
+	logging.debug('Set URL')
+	port.write(('AT+HTTPPARA=\"URL\",\"https://api.thingspeak.com/update?api_key=VSKIFNK3X986380X&field1=' +str(timestmp)+ '&field2=' +str(latitude) + '&field3=' +str(lat_dir)+ '&field4=' +str(longitude) +'&field5=' +str(lon_dir)+'&field6='+str(altitude)+'&field7=' +str(gps_num_sats)+'&field8=' +str(speed)+'\"\r\n').encode())
+	print("data send")
+	logging.debug('URL SET:' + (port.read(400).decode('utf-8', errors='replace')))
+	response = port.read(100).decode()
+	logging.debug(response)
+	time.sleep(1)
+
+
+
+	logging.debug('HTTP GET Set')
+	port.write('AT+HTTPACTION=0\r\n'.encode())
+	response = port.read(54).decode('utf-8', errors='replace')
+	logging.debug(response)
+	logging.debug('Read Response')
+	port.write('AT+HTTPREAD\r\n'.encode())
+	response = port.read(87).decode('utf-8', errors='replace')
+	logging.debug(response)
+#	logging.debug('Length:' + str(len(response)))
+
+	logging.debug('Restart GPS')
+	port.write("AT+CGNSTST=1\r\n".encode())
+	response = port.readline().decode("utf-8",errors='replace')
+	logging.debug(response)
+	response = port.read(100).decode("utf-8",errors='replace')
+	logging.debug(response)
+
+	while "OK" not in (response):
+		logging.error('GPS Stop')
+		port.write("AT+CGNSTST=0\r\n".encode())
+		response = port.read(300).decode('utf-8', errors='replace')
+		logging.debug(response)
+		time.sleep(1)
+		logging.error('GPS Restart')
+		port.write("AT+CGNSTST=1\r\n".encode())
+		response = port.read(300).decode('utf-8', errors='replace')
+		logging.debug(response)
+		time.sleep(1)
+
+	time.sleep(30)
+
+if __name__ == '__main__':
+
+	GGAmsg = None # location info
+	RMCmsg = None # speed info
+	try:
+		while True:
+			line = port.readline()
+			logging.debug(line)
+			line = line.strip()
+			try:
+				line = line.decode("utf-8")
+				msg = pynmea2.parse(line, check=False)
+				if isinstance(msg, pynmea2.types.talker.GGA):
+					GGAmsg = msg
+				elif isinstance(msg, pynmea2.types.talker.RMC):
+					RMCmsg = msg
+			except pynmea2.nmea.ChecksumError:
+				print('ignoring checksum error: ' + str(line))
+			except pynmea2.nmea.ParseError:
+				print('ignoring parse error: ' + str(line))
+			except UnicodeDecodeError:
+				print('ignoring unicode error: ' + str(line))
+
+			if GGAmsg and RMCmsg:
+				handleGPSmsg(GGAmsg, RMCmsg)
+				GGAmsg = None; RMCmsg = None
+
+	finally:
+		port.close()
